24813fffb69c56fb95faecb5ae289a6bb2e3b8f5
Samuel Hiroyuki <36797629+SamuelHiroyuki@users.noreply.github.com>
2019-05-11 17:33:55 -0300

    Validações

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 const routes = require('express').Router()
+const validate = require('express-validation')
+
+const Validators = require('./app/validators')
 
 // Controllers
 const Controllers = require('./app/controllers')
@@ -8,21 +11,33 @@ const Controllers = require('./app/controllers')
 const authMiddleware = require('./app/middlewares/Auth')
 //
 
-routes.post('/tokens', Controllers.SessionController.create)
+routes.post(
+  '/tokens',
+  validate(Validators.Session),
+  Controllers.SessionController.create
+)
 
 routes.use(authMiddleware)
 
 // Users
-routes.post('/users', Controllers.UserController.create)
+routes.post(
+  '/users',
+  validate(Validators.User),
+  Controllers.UserController.create
+)
 
 // Ad
 routes.get('/ads', Controllers.AdController.get)
 routes.get('/ads/:id', Controllers.AdController.getById)
-routes.post('/ads', Controllers.AdController.create)
-routes.put('/ads/:id', Controllers.AdController.update)
+routes.post('/ads', validate(Validators.Ad), Controllers.AdController.create)
+routes.put('/ads/:id', validate(Validators.Ad), Controllers.AdController.update)
 routes.delete('/ads/:id', Controllers.AdController.delete)
 
 // Purchase
-routes.post('/purchases', Controllers.PurchaseController.create)
+routes.post(
+  '/purchases',
+  validate(Validators.Purchase),
+  Controllers.PurchaseController.create
+)
 
 module.exports = routes
